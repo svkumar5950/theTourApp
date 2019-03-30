ec66430d4ebcb086003d1e60a6b6fbf13c5c7740
svkumar5950 <svkumar5950@gmail.com>
2019-03-30 20:02:56 +0100

    Done some remodelling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
 import Header from "./Layouts/Header";
-import About from "./Components/About";
-
 import "./App.scss";
+import Home from "./Components/Home";
 
 class App extends Component {
-  state = {};
+  state = {
+    data: []
+  };
+  async componentDidMount() {
+    const data = await await fetch("./data/tourdata.json").then(res =>
+      res.json()
+    );
+    this.setState({ data: data.details });
+  }
   render() {
     return (
       <div className="App">
         <Header />
-        <About />
+        <Home data={this.state.data} />
       </div>
     );
   }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,21 +1,22 @@
 import React, { Fragment } from "react";
-
-export default function Home() {
+export default function Home(props) {
+  const { data } = props;
   return (
     <Fragment>
       <div className="app-home-sections">
-        <a href="/">
-          <div className="home-img">
-            <img src="http://placeimg.com/400/250/any" alt="Unsplash" />
-            <h3>The Heading For the Image</h3>
-            <p>
-              Nothing's gonna make your husband or wife madder than coming home
-              and having a snow-covered dinner. All you have to do is let your
-              imagination go wild. Nature is so fantastic, enjoy it. Let it make
-              you happy.
-            </p>
-          </div>
-        </a>
+        {data.map(data => {
+          const { id, title, description, image, place } = data;
+          return (
+            <div className="app-home">
+              <div key={id} className="home-section">
+                <img src={image} alt="Trip show off" />
+                <h1>{title}</h1>
+                <h3>{place}</h3>
+                <p>{description}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </Fragment>
   );
